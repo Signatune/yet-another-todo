130cdc09b139fd3409ca82ca97a9f44ca1c65b91
Mike <michaelambrosino93@gmail.com>
2023-09-15 12:14:08 -0400

    configure webpack to minimum functionality

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Yet Another Todo",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+  },
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/,
+  },
+  devtool: "inline-source-map",
+  optimization: {
+    runtimeChunk: "single",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
